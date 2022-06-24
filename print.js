11f6cf154211d39c9c1d44a3a94f0281a7d04f6d
Oluwasegun Adedeji <64655766+Hephzihub@users.noreply.github.com>
2022-06-24 13:45:52 +0100

    Update print.js

--- print.js
+++ print.js
@@ -2,4 +2,4 @@ var name = "Oluwasegun Adedeji";
 var height = "5 Inch";
 var country = "Nigeria";
 
-alert("hi, my name is " + name + " , I'm " + height + ". I'm from " + country + ".");
+alert("hi, my name is " + name + " , I'm " + height + " tall. I'm from " + country + ".");
